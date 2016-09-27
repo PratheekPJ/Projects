60a60cf7e281604e29c04c9b3a355f1da9b91cdb
PratheePJ <pratheekpj2431998@gmail.com>
2016-09-27 20:56:43 +0530

    adding files

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.user.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+}
